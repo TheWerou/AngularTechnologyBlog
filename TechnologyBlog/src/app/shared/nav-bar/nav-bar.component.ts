c9c3c4983dcbcc1be5e6b651dad2328deeb10fe9
Wojciech Buńka <wojtek.bunka@gmail.com>
2021-04-18 18:52:58 +0200

    BIG improvements

--- TechnologyBlog/src/app/shared/nav-bar/nav-bar.component.ts
+++ TechnologyBlog/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import  {DataBaseService } from '/src/app/services/data-base.service'
-import { Section, Post, Category, User,MainSite } from './src/app/shared/data/InterFaces/InterFaces';
+import { DataBaseService } from 'src/app/services/data-base.service'
+import { Section, Post, Category, User, MainSite } from 'src/app/shared/data/InterFaces/InterFaces';
+import { faSearch, faUser, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,10 +11,17 @@ import { Section, Post, Category, User,MainSite } from './src/app/shared/data/In
 export class NavBarComponent implements OnInit {
 
   constructor(private backend: DataBaseService) { }
+  iconSearch = faSearch;
+  iconUser = faUser;
+  iconPlus = faPlus;
+  
   categoryList: Category[];
+
   ngOnInit(): void {
+    this.list();
   }
-  public list(){
+
+  public list() {
     this.categoryList = this.backend.getCategory();
-   }
+  }
 }
